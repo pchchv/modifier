d71fc973cdea39a403d2f9b5422b11b7fd5499dc
pchchv <ipchchv@gmail.com>
2025-06-16 20:30:35 +0300

    Add StructLevel interface for struct level modifier functions

---
+++ struct_level.go
@@ -0,0 +1,17 @@
+package modifier
+
+import "reflect"
+
+// StructLevel represents the interface for struct level modifier function
+type StructLevel interface {
+	// Transformer represents a subset of the current *Transformer that is executing the current transformation.
+	Transformer() Transform
+	// Parent returns the top level parent of the current value return by Struct().
+	// This is used primarily for having the ability to nil out pointer type values.
+	// NOTE: that is there are several layers of abstractions
+	//       e. g. interface{} of interface{} of interface{} this
+	//       function returns the first interface{}.
+	Parent() reflect.Value
+	// Struct returns the value of the current struct being modified.
+	Struct() reflect.Value
+}
