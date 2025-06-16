2fa16c2b9260b3ce03103908c406673152414892
pchchv <ipchchv@gmail.com>
2025-06-16 20:23:55 +0300

    Add InterceptorFunc type for custom type interception

--- modifier.go
+++ modifier.go
@@ -1,2 +1,7 @@
 package modifier
 
+import "reflect"
+
+// InterceptorFunc is a way to intercept custom types to redirect the functions to be applied to an inner typ/value.
+// E. g. sql.NullString, the manipulation should be done on the inner string.
+type InterceptorFunc func(current reflect.Value) (inner reflect.Value)
