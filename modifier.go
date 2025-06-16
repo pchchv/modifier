cc7ab6e67f5867a0d54029bd3e6ce4ae4bf4e3b5
pchchv <ipchchv@gmail.com>
2025-06-16 20:28:54 +0300

    Add Func type for defining transform functions with context and field level

--- modifier.go
+++ modifier.go
@@ -9,6 +9,9 @@ import (
 // E. g. sql.NullString, the manipulation should be done on the inner string.
 type InterceptorFunc func(current reflect.Value) (inner reflect.Value)
 
+// Func defines a transform function for use.
+type Func func(ctx context.Context, fl FieldLevel) error
+
 // Transform represents a subset of the
 // current *Transformer that is executing the
 // current transformation.
