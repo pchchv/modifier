0a59b4f30c09039ff404a849b81f5cf7c434a371
pchchv <ipchchv@gmail.com>
2025-06-17 20:43:48 +0300

    Add error variables for invalid dive and keys tag configurations

--- errors.go
+++ errors.go
@@ -1,8 +1,18 @@
 package modifier
 
 import (
+	"errors"
 	"fmt"
 	"strings"
+)
+
+var (
+	// ErrInvalidDive describes an invalid dive tag configuration.
+	ErrInvalidDive = errors.New("invalid dive tag configuration")
+	// ErrInvalidKeysTag describes a misuse of the keys tag.
+	ErrInvalidKeysTag = errors.New("'" + keysTag + "' tag must be immediately preceeded by the '" + diveTag + "' tag")
+	// ErrUndefinedKeysTag describes an undefined keys tag when and endkeys tag defined.
+	ErrUndefinedKeysTag = errors.New("'" + endKeysTag + "' tag encountered without a corresponding '" + keysTag + "' tag")
 )
 
 // ErrInvalidTag defines a bad value for a tag being used.
