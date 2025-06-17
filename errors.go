ec436afc43a870c4c41d7df1eded427548ff206e
pchchv <ipchchv@gmail.com>
2025-06-17 20:41:02 +0300

    Implement Error method for ErrInvalidTag to provide detailed error messages

--- errors.go
+++ errors.go
@@ -1,7 +1,14 @@
 package modifier
 
+import "fmt"
+
 // ErrInvalidTag defines a bad value for a tag being used.
 type ErrInvalidTag struct {
 	tag   string
 	field string
+}
+
+// Error returns the InvalidTag error text.
+func (e *ErrInvalidTag) Error() string {
+	return fmt.Sprintf("invalid tag '%s' found on field %s", e.tag, e.field)
 }
