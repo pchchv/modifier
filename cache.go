fd1a8d572ab1787ac4850019770cedaf3b7065a9
pchchv <ipchchv@gmail.com>
2025-06-17 20:47:18 +0300

    Implement recursive parsing for field tags in Transformer

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package modifier
 
 import (
 	"reflect"
+	"strings"
 	"sync"
 	"sync/atomic"
 )
@@ -78,4 +79,108 @@ func (tc *tagCache) Set(key string, value *cTag) {
 
 	nm[key] = value
 	tc.m.Store(nm)
+}
+
+func (t *Transformer) parseFieldTagsRecursive(tag string, fieldName string, alias string, hasAlias bool) (firstCtag *cTag, current *cTag, err error) {
+	var tg string
+	var ok bool
+	noAlias := len(alias) == 0
+	tags := strings.Split(tag, tagSeparator)
+	for i := 0; i < len(tags); i++ {
+		tg = tags[i]
+		if noAlias {
+			alias = tg
+		}
+
+		// check map for alias and process new tags, otherwise process as usual
+		if tagsVal, found := t.aliases[tg]; found {
+			if i == 0 {
+				firstCtag, current, err = t.parseFieldTagsRecursive(tagsVal, fieldName, tg, true)
+				if err != nil {
+					return
+				}
+			} else {
+				next, curr, e := t.parseFieldTagsRecursive(tagsVal, fieldName, tg, true)
+				if e != nil {
+					err = e
+					return
+				}
+
+				current.next, current = next, curr
+			}
+			continue
+		}
+
+		var prevTag tagType
+		if i == 0 {
+			current = &cTag{aliasTag: alias, hasAlias: hasAlias, hasTag: true}
+			firstCtag = current
+		} else {
+			prevTag = current.typeof
+			current.next = &cTag{aliasTag: alias, hasAlias: hasAlias, hasTag: true}
+			current = current.next
+		}
+
+		switch tg {
+		case diveTag:
+			current.typeof = typeDive
+			continue
+		case keysTag:
+			current.typeof = typeKeys
+			if i == 0 || prevTag != typeDive {
+				err = ErrInvalidKeysTag
+				return
+			}
+
+			current.typeof = typeKeys
+			// need to pass along only keys tag
+			// need to increment i to skip over the keys tags
+			b := make([]byte, 0, 64)
+			i++
+			for ; i < len(tags); i++ {
+				b = append(append(b, tags[i]...), ',')
+				if tags[i] == endKeysTag {
+					break
+				}
+			}
+
+			if current.keys, _, err = t.parseFieldTagsRecursive(string(b[:len(b)-1]), fieldName, "", false); err != nil {
+				return
+			}
+
+			continue
+		case endKeysTag:
+			current.typeof = typeEndKeys
+			// if there are more in tags then there was no keysTag defined and an error should be thrown
+			if i != len(tags)-1 {
+				err = ErrUndefinedKeysTag
+			}
+			return
+		default:
+			vals := strings.SplitN(tg, tagKeySeparator, 2)
+			if noAlias {
+				alias = vals[0]
+				current.aliasTag = alias
+			} else {
+				current.actualAliasTag = tg
+			}
+
+			current.tag = vals[0]
+			if len(current.tag) == 0 {
+				err = &ErrInvalidTag{tag: current.tag, field: fieldName}
+				return
+			}
+
+			if current.fn, ok = t.transformations[current.tag]; !ok {
+				err = &ErrUndefinedTag{tag: current.tag, field: fieldName}
+				return
+			}
+
+			if len(vals) > 1 {
+				current.param = strings.Replace(vals[1], utf8HexComma, ",", -1)
+			}
+		}
+	}
+
+	return
 }
