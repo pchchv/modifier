c4973de025576930699fb6eadf670c37d9e04cd4
pchchv <ipchchv@gmail.com>
2025-06-17 20:44:16 +0300

    Add initial constants for tag handling in the modifier package

---
+++ restricted.go
@@ -0,0 +1,10 @@
+package modifier
+
+const (
+	diveTag         = "dive"
+	keysTag         = "keys"
+	endKeysTag      = "endkeys"
+	utf8HexComma    = "0x2C"
+	tagSeparator    = ","
+	tagKeySeparator = "="
+)
