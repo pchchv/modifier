b0d3586f67d40d7bbddc2a9d0c914ac0a92e9b09
pchchv <ipchchv@gmail.com>
2025-06-18 19:26:35 +0300

    Add tests for struct and array handling in transformer

--- modifier_test.go
+++ modifier_test.go
@@ -2,6 +2,8 @@ package modifier
 
 import (
 	"context"
+	"errors"
+	"reflect"
 	"testing"
 	"time"
 
@@ -64,4 +66,106 @@ func TestBadValues(t *testing.T) {
 	PanicMatches(t, func() { tform.RegisterAlias("", "") }, "Alias cannot be empty")
 	PanicMatches(t, func() { tform.RegisterAlias("test", "") }, "Aliased tags cannot be empty")
 	PanicMatches(t, func() { tform.RegisterAlias(",", "test") }, "Alias ',' either contains restricted characters or is the same as a restricted tag needed for normal operation")
+}
+
+func TestStructArray(t *testing.T) {
+	type InnerStruct struct {
+		String string `s:"defaultStr"`
+	}
+
+	type Test struct {
+		Inner    InnerStruct
+		Arr      []InnerStruct `s:"defaultArr"`
+		ArrDive  []InnerStruct `s:"defaultArr,dive"`
+		ArrNoTag []InnerStruct
+	}
+
+	set := New()
+	set.SetTagName("s")
+	set.Register("defaultArr", func(ctx context.Context, fl FieldLevel) error {
+		if HasValue(fl.Field()) {
+			return nil
+		}
+		fl.Field().Set(reflect.MakeSlice(fl.Field().Type(), 2, 2))
+		return nil
+	})
+	set.Register("defaultStr", func(ctx context.Context, fl FieldLevel) error {
+		if fl.Field().String() == "ok" {
+			return errors.New("ALREADY OK")
+		}
+		fl.Field().SetString("default")
+		return nil
+	})
+
+	var tt Test
+	err := set.Struct(context.Background(), &tt)
+	Equal(t, err, nil)
+	Equal(t, len(tt.Arr), 2)
+	Equal(t, len(tt.ArrDive), 2)
+	Equal(t, tt.Arr[0].String, "")
+	Equal(t, tt.Arr[1].String, "")
+	Equal(t, tt.ArrDive[0].String, "default")
+	Equal(t, tt.ArrDive[1].String, "default")
+	Equal(t, tt.Inner.String, "default")
+
+	tt2 := Test{
+		Arr: make([]InnerStruct, 1),
+	}
+
+	err = set.Struct(context.Background(), &tt2)
+	Equal(t, err, nil)
+	Equal(t, len(tt2.Arr), 1)
+	Equal(t, tt2.Arr[0].String, "")
+
+	tt3 := Test{
+		Arr: []InnerStruct{{"ok"}},
+	}
+
+	err = set.Struct(context.Background(), &tt3)
+	Equal(t, err, nil)
+	Equal(t, len(tt3.Arr), 1)
+	Equal(t, tt3.Arr[0].String, "ok")
+
+	tt4 := Test{
+		ArrDive: []InnerStruct{{"ok"}},
+	}
+
+	err = set.Struct(context.Background(), &tt4)
+	NotEqual(t, err, nil)
+	Equal(t, err.Error(), "ALREADY OK")
+
+	tt5 := Test{
+		ArrNoTag: make([]InnerStruct, 1),
+	}
+
+	err = set.Struct(context.Background(), &tt5)
+	Equal(t, err, nil)
+	Equal(t, len(tt5.ArrNoTag), 1)
+	Equal(t, tt5.ArrNoTag[0].String, "")
+}
+
+func TestStructLevel(t *testing.T) {
+	type Test struct {
+		String string
+	}
+
+	set := New()
+	set.RegisterStructLevel(func(ctx context.Context, sl StructLevel) error {
+		s := sl.Struct().Interface().(Test)
+		if s.String == "error" {
+			return errors.New("BAD VALUE")
+		}
+		s.String = "test"
+		sl.Struct().Set(reflect.ValueOf(s))
+		return nil
+	}, Test{})
+
+	var tt Test
+	err := set.Struct(context.Background(), &tt)
+	Equal(t, err, nil)
+	Equal(t, tt.String, "test")
+
+	tt.String = "error"
+	err = set.Struct(context.Background(), &tt)
+	NotEqual(t, err, nil)
 }
