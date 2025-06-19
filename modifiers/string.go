0ec06a7fe40d54072b4f577577be109327df4f2e
pchchv <ipchchv@gmail.com>
2025-06-19 20:09:53 +0300

    Implement string conversion functions: snakeCase, slugCase, and titleCase

--- modifiers/string.go
+++ modifiers/string.go
@@ -8,7 +8,11 @@ import (
 	"unicode"
 	"unicode/utf8"
 
+	"github.com/gosimple/slug"
 	"github.com/pchchv/modifier"
+	"github.com/segmentio/go-snakecase"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 // trimLeft trims extra left hand side of string using provided cutset.
@@ -95,5 +99,33 @@ func uppercaseFirstCharacterCase(_ context.Context, fl modifier.FieldLevel) erro
 		fl.Field().SetString(buf.String())
 	}
 
+	return nil
+}
+
+// snakeCase converts string to snake case.
+func snakeCase(ctx context.Context, fl modifier.FieldLevel) error {
+	switch fl.Field().Kind() {
+	case reflect.String:
+		fl.Field().SetString(snakecase.Snakecase(fl.Field().String()))
+	}
+	return nil
+}
+
+// slug converts string to a slug.
+func slugCase(ctx context.Context, fl modifier.FieldLevel) error {
+	switch fl.Field().Kind() {
+	case reflect.String:
+		fl.Field().SetString(slug.Make(fl.Field().String()))
+	}
+	return nil
+}
+
+// titleCase converts string to title case,
+// e.g. "this is a sentence" -> "This Is A Sentence".
+func titleCase(ctx context.Context, fl modifier.FieldLevel) error {
+	switch fl.Field().Kind() {
+	case reflect.String:
+		fl.Field().SetString(cases.Title(language.Und, cases.NoLower).String(fl.Field().String()))
+	}
 	return nil
 }
