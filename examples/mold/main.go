825d776f0686fcff06757c217364e83ccff4def2
pchchv <ipchchv@gmail.com>
2025-06-20 18:15:55 +0300

    Implement main function to demonstrate transformer usage with struct and field

--- examples/mold/main.go
+++ examples/mold/main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/pchchv/modifier"
 )
 
-func main() {}
+var tform *modifier.Transformer
+
+func main() {
+	type Test struct {
+		String string `modifier:"set"`
+	}
+
+	var tt Test
+	tform = modifier.New()
+	tform.Register("set", transformData)
+	if err := tform.Struct(context.Background(), &tt); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", tt)
+
+	var str string
+	if err := tform.Field(context.Background(), &str, "set"); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(str)
+}
 
 func transformData(ctx context.Context, fl modifier.FieldLevel) error {
 	fl.Field().SetString("test")
