f62f7d95c5a17e298505d0bbe79c93d61ae4e6a4
pchchv <ipchchv@gmail.com>
2025-06-20 18:06:52 +0300

    Implement New function scrubber initialization

--- scrubbers/scrubbers.go
+++ scrubbers/scrubbers.go
@@ -1,1 +1,16 @@
 package scrubbers
+
+import "github.com/pchchv/modifier"
+
+// New returns a scrubber with defaults registered.
+func New() *modifier.Transformer {
+	scrub := modifier.New()
+	scrub.SetTagName("scrub")
+	scrub.Register("emails", emails)
+	scrub.Register("text", textFn("text"))
+	scrub.Register("email", textFn("email"))
+	scrub.Register("name", textFn("name"))
+	scrub.Register("fname", textFn("fname"))
+	scrub.Register("lname", textFn("lname"))
+	return scrub
+}
