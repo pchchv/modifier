35214ef874aba60518c6a8d00bf235cb0231056f
pchchv <ipchchv@gmail.com>
2025-06-20 18:14:17 +0300

    Implement transformData function to modify field values

--- examples/mold/main.go
+++ examples/mold/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"context"
+
+	"github.com/pchchv/modifier"
+)
+
+func main() {}
+
+func transformData(ctx context.Context, fl modifier.FieldLevel) error {
+	fl.Field().SetString("test")
+	return nil
+}
