713288e240d0c46a56af896c49fad4a88d29eb20
pchchv <ipchchv@gmail.com>
2025-06-23 21:22:42 +0300

    Add Address type with validation and trimming modifiers

--- examples/full/main.go
+++ examples/full/main.go
@@ -1,3 +1,9 @@
 package main
 
+// Address contains address information.
+type Address struct {
+	Name  string `mod:"trim" validate:"required"`
+	Phone string `mod:"trim" validate:"required"`
+}
+
 func main() {}
