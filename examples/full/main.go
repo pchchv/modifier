0e5799b1b30ce56dfcb7276bd0e927f9944c5c3b
pchchv <ipchchv@gmail.com>
2025-06-23 21:23:30 +0300

    Add User type with validation and trimming modifiers

--- examples/full/main.go
+++ examples/full/main.go
@@ -6,4 +6,15 @@ type Address struct {
 	Phone string `mod:"trim" validate:"required"`
 }
 
+// User contains user information.
+type User struct {
+	Name    string            `mod:"trim"      validate:"required"              scrub:"name"`
+	Age     uint8             `                validate:"required,gt=0,lt=130"`
+	Gender  string            `                validate:"required"`
+	Email   string            `mod:"trim"      validate:"required,email"        scrub:"emails"`
+	Address []Address         `                validate:"required,dive"`
+	Active  bool              `form:"active"`
+	Misc    map[string]string `mod:"dive,keys,trim,endkeys,trim"`
+}
+
 func main() {}
