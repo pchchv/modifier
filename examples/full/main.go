086c1e1859e2411e66edcb02d1019d7c3fa2e6b5
pchchv <ipchchv@gmail.com>
2025-06-23 21:34:38 +0300

    Implement main function to decode, conform, validate, and scrub user data

--- examples/full/main.go
+++ examples/full/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "net/url"
+import (
+	"context"
+	"log"
+	"net/url"
+
+	"github.com/pchchv/form"
+	"github.com/pchchv/modifier/modifiers"
+	"github.com/pchchv/modifier/scrubbers"
+	"github.com/pchchv/validator"
+)
+
+var (
+	decoder  = form.NewDecoder()
+	scrub    = scrubbers.New()
+	conform  = modifiers.New()
+	validate = validator.New()
+)
 
 // Address contains address information.
 type Address struct {
@@ -19,8 +35,6 @@ type User struct {
 	Misc    map[string]string `mod:"dive,keys,trim,endkeys,trim"`
 }
 
-func main() {}
-
 // parseForm simulates the results of http.Request's ParseForm() function.
 func parseForm() url.Values {
 	return url.Values{
@@ -35,4 +49,41 @@ func parseForm() url.Values {
 		"active":           []string{"true"},
 		"Misc[  b4  ]":     []string{"  b4  "},
 	}
+}
+
+func main() {
+	var user User
+	values := parseForm()
+	// must pass a pointer
+	if err := decoder.Decode(&user, values); err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Decoded:%+v\n\n", user)
+
+	// great now lets conform our values,
+	// after all a human input the data nobody's perfect
+	if err := conform.Struct(context.Background(), &user); err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Conformed:%+v\n\n", user)
+
+	// that's better all those extra spaces are gone let's validate the data
+	if err := validate.Struct(user); err != nil {
+		log.Panic(err)
+	}
+
+	// data's validated, proceeding:
+	// save to database
+	// process request
+	// etc....
+
+	// working with the data is over, let's log it or save it somewhere
+	// there is sensitive PII data, it should be made sure that it is de-identified first
+	if err := scrub.Struct(context.Background(), &user); err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Scrubbed:%+v\n\n", user)
 }
